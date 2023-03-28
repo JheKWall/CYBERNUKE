700b314a52e1f22268fdf0485aa3be29d02ec346
Kenneth Wallace <jhekwall@ut.utm.edu>
2023-03-28 14:02:48 -0500

    Font fixed

--- CYBERNUKE/Properties/Resources.Designer.cs
+++ CYBERNUKE/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace CYBERNUKE.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -143,9 +143,9 @@ namespace CYBERNUKE.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Byte[].
         /// </summary>
-        internal static byte[] unispace_bd {
+        internal static byte[] Unispace {
             get {
-                object obj = ResourceManager.GetObject("unispace_bd", resourceCulture);
+                object obj = ResourceManager.GetObject("Unispace", resourceCulture);
                 return ((byte[])(obj));
             }
         }
