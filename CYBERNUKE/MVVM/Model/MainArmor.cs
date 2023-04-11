fa5a5c3846b660dd9a75a2ad32c628f3cc48b031
Vance Brenderabrandis <vanabren@ut.utm.edu>
2023-04-11 12:38:08 -0500

    added a bunch of concrete classes with constructors that might have been a waste of time to add

---
+++ CYBERNUKE/MVVM/Model/MainArmor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYBERNUKE.MVVM.Model
+{
+    public class MainArmor : Item
+    {
+        public MainArmor() {
+            setName(null);
+            setItemValue(0);
+            setDescription(null);
+            setImage(null);
+            setIsDiscardable(true);
+        }
+
+        public MainArmor(string name, int value, string description, Image image, bool i)
+        {
+            setName(name);
+            setItemValue(value);
+            setDescription(description);
+            setImage(image);
+            setIsDiscardable(i);
+        }
+    }
+}
---
+++ CYBERNUKE/MVVM/Model/MeleeWeapon.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CYBERNUKE.MVVM.Model
+{
+    public class MeleeWeapon : Item
+    {
+        public MeleeWeapon() {
+            setName(null);
+            setItemValue(0);
+            setDescription(null);
+            setImage(null);
+            setIsDiscardable(true);
+        }
+
+        public MeleeWeapon(string name, int value, string description, Image image, bool i)
+        {
+            setName(name);
+            setItemValue(value);
+            setDescription(description);
+            setImage(image);
+            setIsDiscardable(i);
+        }
+    }
+}
